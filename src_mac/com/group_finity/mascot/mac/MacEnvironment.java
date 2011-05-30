208bec59f80e910d5b5bdd3de6545fe16370abce
nonowarn <nonowarn@gmail.com>
2011-05-30 21:55:46 +0900

    起動時ハングするけどとりあえずコミット
    今原因だと思っているのは、しめじを起動すると Frontmost Application がしめじになってしまうこと
    これを確かめるためには、
    
    * Applescript 内で Frontmost Applescript がしめじなら null を返す
    * しめじを System tray のみで動かすようにする (Alfred みたいに)
    
    現在、後者の方針で調査中。どうせやる必要があると思っていたので。

--- src_mac/com/group_finity/mascot/mac/MacEnvironment.java
+++ src_mac/com/group_finity/mascot/mac/MacEnvironment.java
@@ -1,21 +1,67 @@
 package com.group_finity.mascot.mac;
 
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 import com.group_finity.mascot.environment.Area;
 import com.group_finity.mascot.environment.Environment;
 
 /**
- * Java では取得が難しい環境情報をJNIを使用して取得する.
+ * Java では取得が難しい環境情報をAppleScriptを使用して取得する.
  */
 class MacEnvironment extends Environment {
 
 	private static Area activeIE = new Area();
+  private static Area frontmostApp = activeIE;
+  private static ScriptEngine engine = new ScriptEngineManager().getEngineByName("AppleScript");
+
+  private static Rectangle getFrontmostAppRect() {
+    ArrayList<Long> bounds = null;
+
+    try {
+      bounds = (ArrayList<Long>) engine.eval(getFrontmostAppRectScript());
+    } catch (ScriptException e) {}
+
+    if (bounds != null && bounds.size() == 4) {
+      return rectangleFromBounds(bounds);
+    } else {
+      return null;
+    }
+  }
+
+  private static String getFrontmostAppRectScript() {
+    return "tell application \"System Events\"\n" +
+           "  set appName to name of first item of (processes whose frontmost is true)\n" +
+           "end tell\n" +
+           "return bounds of first window of application appName";
+  }
+
+  private static Rectangle rectangleFromBounds(ArrayList<Long> bounds) {
+    int
+      leftTopX     = bounds.get(0).intValue(), leftTopY     = bounds.get(1).intValue(),
+      rightBottomX = bounds.get(2).intValue(), rightBottomY = bounds.get(3).intValue();
+    return new Rectangle(
+      leftTopX,
+      leftTopY,
+      rightBottomX - leftTopX,
+      rightBottomY - leftTopY);
+  }
 
 	@Override
 	public void tick() {
 		super.tick();
-		this.activeIE.setVisible(false);
+    final Rectangle frontmostAppRect = getFrontmostAppRect();
+
+    frontmostApp.setVisible(
+      (frontmostAppRect != null)
+      && frontmostAppRect.intersects(getScreen().toRectangle()));
+    frontmostApp.set(
+      frontmostAppRect == null ? new Rectangle(-1, -1, 0, 0) : frontmostAppRect);
 	}
 
 	@Override
@@ -24,7 +70,6 @@ class MacEnvironment extends Environment {
 
 	@Override
 	public void restoreIE() {
-
 	}
 
 	@Override
